bed7d9c5a41cf0f5ca91c522454e9badfd9ae1e8
Alumno <Alumno@ws204-18>
2019-02-06 17:59:48 -0800

    comentario cuando se realiza cambio,  es obligatorio poner el mensaje

---
+++ Paquetes/src/pruebas/test.java
@@ -0,0 +1,19 @@
+package pruebas;
+
+//import paquete1.Persona;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	//	Persona p1=new Persona(24,"karen");
+		 paquete1.Persona p=new paquete1.Persona(24,"karen");
+		 paquete2.Persona p2=new paquete2.Persona(24,"karen","Femenino");
+		
+		 
+		 System.out.println(p.toString());
+		 System.out.println(p2.toString());
+	}
+
+}
